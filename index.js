22f4ecbfc89c1706f9328f8a439b9de432eef6c1
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-06-22 19:51:58 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update ESLint form v4 to v5 https://eslint.org/docs/user-guide/migrating-to-5.0.0
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -5,44 +5,43 @@ const inspectWithKind = require('inspect-with-kind');
 const isPlainObject = require('lodash/isPlainObject');
 
 module.exports = function configuredESLintRules(...args) {
-  const argLen = args.length;
+	const argLen = args.length;
 
-  if (argLen > 2) {
-    throw new RangeError(`Expected 0, 1 or 2 arguments ([<string>][, <Object>]), but got ${argLen} arguments.`);
-  }
+	if (argLen > 2) {
+		throw new RangeError(`Expected 0, 1 or 2 arguments ([<string>][, <Object>]), but got ${argLen} arguments.`);
+	}
 
-  if (argLen === 0) {
-    return Object.keys((new CLIEngine()).getConfigForFile().rules);
-  }
+	if (argLen === 0) {
+		return Object.keys((new CLIEngine()).getConfigForFile().rules);
+	}
 
-  if (argLen === 1) {
-    if (typeof args[0] === 'string') {
-      return Object.keys((new CLIEngine()).getConfigForFile(args[0]).rules);
-    }
+	if (argLen === 1) {
+		if (typeof args[0] === 'string') {
+			return Object.keys((new CLIEngine()).getConfigForFile(args[0]).rules);
+		}
 
-    if (!isPlainObject(args[0])) {
-      throw new TypeError(`Expected the first argument to be a string or Object, but got ${
-        inspectWithKind(args[0])
-      }.`);
-    }
+		if (!isPlainObject(args[0])) {
+			throw new TypeError(`Expected the first argument to be a string or Object, but got ${
+				inspectWithKind(args[0])
+			}.`);
+		}
 
-    return Object.keys((new CLIEngine(args[0])).getConfigForFile().rules);
-  }
+		return Object.keys((new CLIEngine(args[0])).getConfigForFile().rules);
+	}
 
-  const [filePath, options] = args;
+	const [filePath, options] = args;
 
-  if (typeof filePath !== 'string') {
-    throw new TypeError(`Expected a file path (string), but got a non-string value ${
-      inspectWithKind(filePath)
-    }.`);
-  }
+	if (typeof filePath !== 'string') {
+		throw new TypeError(`Expected a file path (<string>), but got a non-string value ${
+			inspectWithKind(filePath)
+		}.`);
+	}
 
-  if (!isPlainObject(options)) {
-    throw new TypeError(
-      String(options) +
-      ' is not an object. Second argument must be an ESLint\'s CLI engine option object.'
-    );
-  }
+	if (!isPlainObject(options)) {
+		throw new TypeError(`Expected an ESLint's CLIEngine options object (<Object>), but got ${
+			inspectWithKind(options)
+		}.`);
+	}
 
-  return Object.keys((new CLIEngine(options)).getConfigForFile(filePath).rules);
+	return Object.keys((new CLIEngine(options)).getConfigForFile(filePath).rules);
 };
